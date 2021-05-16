ecc0cc3e66f4efbf492725d4ab70239cda73b010
Paul Kinsella <kince23@gmail.com>
2021-05-16 22:20:42 +0100

    Update1

--- src/Overview/Overview.js
+++ src/Overview/Overview.js
@@ -1,12 +1,19 @@
 import React from "react";
+import "./Overview.css";
+import { NavLink } from "react-router-dom";
 import DesktopCard from '../DesktopCard/DesktopCard';
-const Overview = () => {
-    return <div className="container">
-        <div className="headerSection">Todo</div>
-        <DesktopCard>
+import overview from '../images/overview.PNG';
 
-        </DesktopCard>
-    </div>;
+const Overview = () => {
+  return <div className="container">
+    <div className="headerSection">Overview</div>
+    <DesktopCard>
+      <NavLink exact activeClassName="current" to='./'>
+        <i class="fa fa-home fa-2x" aria-hidden="true"></i>
+      </NavLink>
+      <img src={overview} alt="overview" />
+    </DesktopCard>
+  </div>;
 };
 
 export default Overview;
